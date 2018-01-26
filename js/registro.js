2af24cef3146ae03122fe9e4e19c5421679fbbe2
Droxk <dvillaverdeprado@gmail.com>
2018-01-26 13:59:42 +0100

    Validaciones añadidas

--- js/registro.js
+++ js/registro.js
@@ -1,12 +1,28 @@
+// Identificador
 var idRegSelect;
+var idDiv;
+// Nombre completo
 var nombreDiv;
+var nombreInput;
+// ICM
 var icmDiv;
+var icmInput;
+// Departamento
 var departamentoDiv;
 var departamentoSelect;
+// Email
 var emailDiv;
+var emailInput;
+// Clave
 var claveDiv;
+var claveInput;
+// Clave repetida
 var claverepDiv;
+var claverepInput;
+// Boton submit registro
+var submitRegistro;
 
+// Generacion de options para los usuarios de registro / modificacion
 function generarOptionsRegistro(){
 	var optionCreado = document.createElement("option");
 	idRegSelect.options[i] = (new Option("ds00"+i, "ds00"+i));
@@ -19,40 +35,115 @@ function generarOptionsRegistro(){
 	}
 }
 
-// probando añadir una clase para la validacion
-function testError(){
-	claveDiv.className += " has-error";
+// VALIDACIONES ------------------------------------------
+
+// Comprueba si el campo esta vacio para añadirle la clase " has-error"
+// ATENCION: Se debe pasar el DIV donde esta el input ya que lo que se edita es la clase
+function compruebaVacio(campo) {
+    if (campo.value == "") {
+		console.log("entra");
+        campo.className += " has-error";
+        return false;
+    }
 }
+
+// Validacion del select de usuario de Registro / Modificacion
+function validaIdSelect(){
+	if(idRegSelect.selectedIndex < 1){
+		idDiv.className = "col-md-6 form-group has-error";
+		return false;
+	} else {
+		idDiv.className = "col-md-6 form-group";
+		return true;
+	}
+}
+
+// Validacion de nombre completo
+function validaNombre(){
+	var re = /^([a-zA-Z]+[\'\,\.\-]?[a-zA-Z ]*)+[ ]([a-zA-Z]+[\'\,\.\-]?[a-zA-Z ]+)+$/;
+	if(!re.test(nombreInput.value)){
+		nombreDiv.className = "col-md-6 form-group has-error";
+		return false;
+	} else {
+		nombreDiv.className = "col-md-6 form-group has-success";
+		return true;
+	}
+}
+
+// Validacion de email
+function validaEmail() {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(!re.test(emailInput.value)){
+		emailDiv.className = "col-md-6 form-group has-error";
+		return false;
+	} else {
+		emailDiv.className = "col-md-6 form-group has-success";
+		return true;
+	}
+}
+
+// Validacion de ICM
+function validaIcm(){
+	var re = /[a-z|A-Z][a-z|A-Z][a-z|A-Z][0-9]?[0-9]?/;
+	if(!re.test(icmInput.value)){
+		icmDiv.className = "col-md-6 form-group has-error";
+		return false;
+	} else {
+		icmDiv.className = "col-md-6 form-group has-success";
+		return true;
+	}
+}
+
+// Validacion de Departamento
+function validaDepartamento(){
+	if(departamentoSelect.selectedIndex < 1){
+		departamentoDiv.className = "col-md-6 form-group has-error";
+		return false;
+	} else {
+		departamentoDiv.className = "col-md-6 form-group";
+		return true;
+	}
+}
+
+// Validacion de contraseña (no empieza ni acaba por 0, son todo numeros, tiene una longitud de 6)
+function validaPass(){
+	
+}
+
+function validaPassRepetida(){
+	
+}
+
+// FIN VALIDACIONES --------------------------------------
 
 // AJAX ----------------------------------------------------------------------------------
 // Obtener la instancia del objeto XMLHttpRequest creando una variable
 function instanciar(){
 	if(window.XMLHttpRequest) {
-		peticion_http_txt= new XMLHttpRequest();
+		peticion_departamentos= new XMLHttpRequest();
 	} else if (window.ActiveXObject) {
-		peticion_http_txt= new ActiveXObject("Microsoft.XMLHTTP");
+		peticion_departamentos= new ActiveXObject("Microsoft.XMLHTTP");
 	}
 }
 
 // Preparar la funcion de respuesta
 function prepararPeticion(){
-	peticion_http_txt.onreadystatechange = obtenerContenido;
+	peticion_departamentos.onreadystatechange = obtenerContenido;
 }
 
 
 // Realizar peticion HTTP
 function realizarPeticion(){
-	peticion_http_txt.open('GET', 'http://192.168.3.176/workspace/aulesco/js/archivos/departamentos.txt', true);
-	peticion_http_txt.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	peticion_http_txt.send(null);
+	peticion_departamentos.open('GET', 'http://192.168.3.176/workspace/aulesco/js/archivos/departamentos.txt', true);
+	peticion_departamentos.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	peticion_departamentos.send(null);
 }
 
 
 function obtenerContenido() {
-	if(peticion_http_txt.readyState == 4) {
-		if(peticion_http_txt.status == 200) {
-			depart_select.innerHTML += peticion_http_txt.responseText;
-			console.log(peticion_http_txt.responseText);
+	if(peticion_departamentos.readyState == 4) {
+		if(peticion_departamentos.status == 200) {
+			depart_select.innerHTML += peticion_departamentos.responseText;
 		}
 	}
 }
@@ -69,19 +160,28 @@ function asignarEventos(){
 	
 	if (document.readyState == 'complete') {
 		idRegSelect = document.getElementById("idreg_select");
-		nombreDiv = document.getElementById("clave_div");
-		icmDiv = document.getElementById("clave_div");
-		departamentoDiv = document.getElementById("clave_div");
+		idDiv = document.getElementById("identificador_div");
+		nombreDiv = document.getElementById("nombre_div");
+		nombreInput = document.getElementById("nombre_input");
+		icmDiv = document.getElementById("icm_div");
+		icmInput = document.getElementById("icm_input");
+		departamentoDiv = document.getElementById("departamento_div");
 		departamentoSelect = document.getElementById("depart_select");
-		emailDiv = document.getElementById("clave_div");
+		emailDiv = document.getElementById("email_div");
+		emailInput = document.getElementById("email_input");
 		claveDiv = document.getElementById("clave_div");
-		claverepDiv = document.getElementById("clave_div");
-		claveDiv = document.getElementById("clave_div");
+		claveInput = document.getElementById("clave_input");
+		claverepDiv = document.getElementById("claverep_div");
+		claverepInput = document.getElementById("claverep_input");
+		submitRegistro = document.getElementById("submit_input");
 		
 		generarOptionsRegistro();
-		testError();
 		
-		//selectUsuario.addEventListener("change", casillasBloqueadas);
+		idRegSelect.addEventListener("change", validaIdSelect);
+		nombreInput.addEventListener("blur", validaNombre);
+		icmInput.addEventListener("blur", validaIcm);
+		emailInput.addEventListener("blur", validaEmail);
+		departamentoSelect.addEventListener("blur", validaDepartamento);
 	}
 }
 
